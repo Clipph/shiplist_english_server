ad4dfcd322d3054d9f848f786f042fe4215a49f6
Clipph <kheijhackt2@gmail.com>
2025-05-25 02:02:25 +0800

    Optimized filtering

--- shiplist/urls.py
+++ shiplist/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ShipViewSet, ship_list_view
+from .views import ShipViewSet, home
 
 router = DefaultRouter()
 router.register(r'ships', ShipViewSet)
 
 urlpatterns = [
-    path('', ship_list_view, name='ship-list'),  # shows homepage at /
-    path('api/', include(router.urls)),          # API at /api/ships/
+    path('', home, name='home'),  # shows homepage at /
+    path('api/', include(router.urls)), # API at /api/ships/
 ]
--- shiplist/views.py
+++ shiplist/views.py
@@ -10,3 +10,19 @@ class ShipViewSet(viewsets.ModelViewSet):
 def ship_list_view(request):
     ships = Ship.objects.all()
     return render(request, 'shiplist/index.html', {'ships': ships})
+
+def pending_ships_view(request):
+        pending_ships = Ship.objects.filter(status=0)
+        return render(request, 'shiplist/pending_ships.html', {'ships': pending_ships})
+
+def untracked_ships_view(request):
+    untracked_ships = Ship.objects.filter(status=1)
+    return render(request, 'shiplist/untracked_ships.html', {'ships': untracked_ships})
+
+def sunken_ships_view(request):
+    sunken_ships = Ship.objects.filter(status=3)
+    return render(request, 'shiplist/sunken_ships.html', {'ships': sunken_ships})
+
+def home(request): # show only sailing ships
+    sailing_ships = Ship.objects.filter(status=2) 
+    return render(request, 'shiplist/index.html', {'ships': sailing_ships})
