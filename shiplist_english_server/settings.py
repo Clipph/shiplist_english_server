8702cfcead905500a90ae37e989874418e13f5f5
Clipph <kheijhackt2@gmail.com>
2025-05-28 01:32:13 +0800

    Updated settings

--- shiplist_english_server/settings.py
+++ shiplist_english_server/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r=5qmvtynp8t#iu(zo5frlemfk6rml37#7i4&r400nu)*&($du'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ShiplistEnglishServer.pythonanywhere.com']
 
 # STATIC SETTINGS
 STATIC_URL = '/static/'
